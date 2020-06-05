9d36ec22ceadc66f24546a23a962f6cea4e85b32
Crowdbotics <team@crowdbotics.com>
2020-06-05 06:49:57 +0000

    Update models

--- users/models.py
+++ users/models.py
@@ -7,6 +7,24 @@ from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=255,)
+    bbfjghgfjhgf = models.OneToOneField(
+        "home.CustomText",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="user_bbfjghgfjhgf",
+    )
+    nbvnbvnbv = models.ManyToManyField(
+        "home.HomePage", blank=True, related_name="user_nbvnbvnbv",
+    )
+    ujgjhgjhg = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="user_ujgjhgjhg",
+    )
+    cvcvcvc = models.BigIntegerField(null=True, blank=True,)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import (
     UserUpdateView,
     UserDetailView,
-    user_redirect_view,
     user_update_view,
+    user_redirect_view,
     user_detail_view,
 )
 
